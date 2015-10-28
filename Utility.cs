9f9da701fd1f5a9f5187f9e8b7c74b128b1a8574
Eric Chauvin <eric7apps@gmail.com>
2015-10-28 09:12:19 -0600

    Update Utility.cs

--- Utility.cs
+++ Utility.cs
@@ -79,6 +79,104 @@ namespace ExampleServer
     }
 
 
+
+  // You could use Base64 instead.
+  internal static string BytesToLetterString( byte[] InBytes )
+    {
+    StringBuilder SBuilder = new StringBuilder();
+    for( int Count = 0; Count < InBytes.Length; Count++ )
+      {
+      uint ByteHigh = InBytes[Count];
+      uint ByteLow = ByteHigh & 0x0F;
+      ByteHigh >>= 4;
+      SBuilder.Append( (char)('A' + (char)ByteHigh) );
+      SBuilder.Append( (char)('A' + (char)ByteLow) );
+      // MForm.ShowStatus( SBuilder.ToString() );
+      }
+
+    return SBuilder.ToString();
+    }
+
+
+
+
+
+
+  private static bool IsInLetterRange( uint Letter )
+    {
+    const uint MaxLetter = (uint)('A') + 15;
+    const uint MinLetter = (uint)'A';
+
+    if( Letter > MaxLetter )
+      {
+      // MForm.ShowStatus( "Letter > MaxLetter" );
+      return false;
+      }
+
+    if( Letter < MinLetter )
+      {
+      // MForm.ShowStatus( "Letter < MinLetter" );
+      return false;
+      }
+
+    return true;
+    }
+
+
+
+
+  internal static byte[] LetterStringToBytes( string InString )
+    {
+    try
+    {
+
+    if( InString == null )
+      return null;
+
+    if( InString.Length < 2 )
+      return null;
+
+    byte[] OutBytes;
+
+    try
+    {
+    OutBytes = new byte[InString.Length >> 1];
+    }
+    catch( Exception )
+      {
+      return null;
+      }
+
+    int Where = 0;
+    for( int Count = 0; Count < OutBytes.Length; Count++ )
+      {
+      uint Letter = InString[Where];
+      if( !IsInLetterRange( Letter ))
+        return null;
+
+      uint ByteHigh = Letter - (uint)'A';
+      ByteHigh <<= 4;
+      Where++;
+      Letter = InString[Where];
+      if( !IsInLetterRange( Letter ))
+        return null;
+
+      uint ByteLow = Letter - (uint)'A';
+      Where++;
+
+      OutBytes[Count] = (byte)(ByteHigh | ByteLow);
+      }
+
+    return OutBytes;
+
+    }
+    catch( Exception )
+      {
+      return null;
+      }
+    }
+
+
   }
 }
 
