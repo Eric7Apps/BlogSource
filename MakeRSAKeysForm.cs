f34798ec2ccce728f3a2859aefb9641d080b1ea3
Eric Chauvin <eric7apps@gmail.com>
2015-12-02 06:12:58 -0700

    Update MakeRSAKeysForm.cs

--- MakeRSAKeysForm.cs
+++ MakeRSAKeysForm.cs
@@ -18,6 +18,8 @@ namespace ExampleServer
     {
     internal CRTCombinSetupRec[] SetupArray;
     internal string PublicKeyModulus;
+    internal uint ModMask;
+    internal string ProcessName;
     }
 
 
@@ -133,18 +135,24 @@ namespace ExampleServer
     {
     MakeKeysWorkerInfo WInfo = new MakeKeysWorkerInfo();
     WInfo.SetupArray = new CRTCombinSetupRec[4];
+    WInfo.ProcessName = "Single thread test";
+    WInfo.PublicKeyModulus = null; // Make a new random one.
 
     WInfo.SetupArray[0].Start = 0;
     WInfo.SetupArray[0].End = 5;
+    WInfo.ModMask = 0xFFFFFFFF;
 
     WInfo.SetupArray[1].Start = 6;
     WInfo.SetupArray[1].End = 7;
+    WInfo.ModMask = 0xFFFFFFFF;
 
     WInfo.SetupArray[2].Start = 8;
     WInfo.SetupArray[2].End = 9;
+    WInfo.ModMask = 0xFFFFFFFF;
 
     WInfo.SetupArray[3].Start = 10;
     WInfo.SetupArray[3].End = 11;
+    WInfo.ModMask = 0xFFFFFFFF;
 
     try
     {
@@ -301,45 +309,12 @@ namespace ExampleServer
     {
     try
     {
-    string PubKey = "15,654,675,243,543,711,381,029";
+    // Some test numbers:
+    // string PubKey =         "15,654,675,243,543,711,381,029";
+    string PubKey =     "28,569,209,393,580,650,250,552,443";
+    // string PubKey = "56,486,207,148,903,090,249,668,503,717";
 
     CRTCombinBackgArray = new CRTCombinBackground[4];
-
-    MakeKeysWorkerInfo WInfo = new MakeKeysWorkerInfo();
-    WInfo.SetupArray = new CRTCombinSetupRec[3];
-    WInfo.PublicKeyModulus = PubKey;
-
-    WInfo.SetupArray[0].Start = 0;
-    WInfo.SetupArray[0].End = 5;
-
-    WInfo.SetupArray[1].Start = 6;
-    WInfo.SetupArray[1].End = 6;
-
-    WInfo.SetupArray[2].Start = 7;
-    WInfo.SetupArray[2].End = 7;
-    CRTCombinBackgArray[0] = new CRTCombinBackground( MForm, WInfo );
-    ShowStatus( "Starting background process at: 0" );
-    CRTCombinBackgArray[0].RunWorkerAsync( WInfo );
-
-
-    ////////////////
-    WInfo = new MakeKeysWorkerInfo();
-    WInfo.SetupArray = new CRTCombinSetupRec[3];
-    WInfo.PublicKeyModulus = PubKey;
-
-    WInfo.SetupArray[0].Start = 0;
-    WInfo.SetupArray[0].End = 3;
-
-    WInfo.SetupArray[1].Start = 4;
-    WInfo.SetupArray[1].End = 4;
-
-    WInfo.SetupArray[2].Start = 5;
-    WInfo.SetupArray[2].End = 5;
-
-    CRTCombinBackgArray[1] = new CRTCombinBackground( MForm, WInfo );
-    ShowStatus( "Starting background process at: 1" );
-    CRTCombinBackgArray[1].RunWorkerAsync( WInfo );
-
 
     // Some primes:
     // 0  1  2  3   4   5   6   7   8   9  10  11  12  13  14  15
@@ -348,22 +323,89 @@ namespace ExampleServer
     // 16  17  18  19  20  21  22  23  24   25   26   27
     // 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107
 
+    // In this case I'm setting up each thread the same, except for the 
+    // ModMask, but you might want to set some threads with smaller
+    // combinations (SetupArray would be smaller and with smaller start
+    // and end values) because if the solutions P and Q were close together
+    // they'd be more likely to be found sooner with smaller combinations.
+
+    int Start0 = 0;
+    int End0 = 6;
+
+    int Start1 = 7;
+    int End1 = 8;
+
+    int Start2 = 9;
+    int End2 = 10;
+
+    int Start3 = 11;
+    int End3 = 12;
+
+    MakeKeysWorkerInfo WInfo = new MakeKeysWorkerInfo();
+    WInfo.SetupArray = new CRTCombinSetupRec[4];
+    WInfo.PublicKeyModulus = PubKey;
+    WInfo.ModMask = 0;
+    WInfo.ProcessName = "Process 0";
+
+    WInfo.SetupArray[0].Start = Start0;
+    WInfo.SetupArray[0].End = End0;
+
+    WInfo.SetupArray[1].Start = Start1;
+    WInfo.SetupArray[1].End = End1;
+
+    WInfo.SetupArray[2].Start = Start2;
+    WInfo.SetupArray[2].End = End2;
+
+    WInfo.SetupArray[3].Start = Start3;
+    WInfo.SetupArray[3].End = End3;
+
+    CRTCombinBackgArray[0] = new CRTCombinBackground( MForm, WInfo );
+    ShowStatus( "Starting background process at: 0" );
+    CRTCombinBackgArray[0].RunWorkerAsync( WInfo );
+
+
+    ////////////////
+    WInfo = new MakeKeysWorkerInfo();
+    WInfo.SetupArray = new CRTCombinSetupRec[4];
+    WInfo.PublicKeyModulus = PubKey;
+    WInfo.ModMask = 1;
+    WInfo.ProcessName = "Process 1";
+
+    WInfo.SetupArray[0].Start = Start0;
+    WInfo.SetupArray[0].End = End0;
+
+    WInfo.SetupArray[1].Start = Start1;
+    WInfo.SetupArray[1].End = End1;
+
+    WInfo.SetupArray[2].Start = Start2;
+    WInfo.SetupArray[2].End = End2;
+
+    WInfo.SetupArray[3].Start = Start3;
+    WInfo.SetupArray[3].End = End3;
+
+    CRTCombinBackgArray[1] = new CRTCombinBackground( MForm, WInfo );
+    ShowStatus( "Starting background process at: 1" );
+    CRTCombinBackgArray[1].RunWorkerAsync( WInfo );
+
+
     ////////
     WInfo = new MakeKeysWorkerInfo();
     WInfo.SetupArray = new CRTCombinSetupRec[4];
     WInfo.PublicKeyModulus = PubKey;
+    WInfo.ModMask = 2;
+    WInfo.ProcessName = "Process 2";
 
-    WInfo.SetupArray[0].Start = 0;
-    WInfo.SetupArray[0].End = 5;
+    WInfo.SetupArray[0].Start = Start0;
+    WInfo.SetupArray[0].End = End0;
 
-    WInfo.SetupArray[1].Start = 6;
-    WInfo.SetupArray[1].End = 6;
+    WInfo.SetupArray[1].Start = Start1;
+    WInfo.SetupArray[1].End = End1;
 
-    WInfo.SetupArray[2].Start = 7;
-    WInfo.SetupArray[2].End = 7;
+    WInfo.SetupArray[2].Start = Start2;
+    WInfo.SetupArray[2].End = End2;
 
-    WInfo.SetupArray[3].Start = 8;
-    WInfo.SetupArray[3].End = 9;
+    WInfo.SetupArray[3].Start = Start3;
+    WInfo.SetupArray[3].End = End3;
 
     CRTCombinBackgArray[2] = new CRTCombinBackground( MForm, WInfo );
     ShowStatus( "Starting background process at: 2" );
@@ -374,27 +416,71 @@ namespace ExampleServer
     WInfo = new MakeKeysWorkerInfo();
     WInfo.SetupArray = new CRTCombinSetupRec[4];
     WInfo.PublicKeyModulus = PubKey;
+    WInfo.ModMask = 3;
+    WInfo.ProcessName = "Process 3";
 
-    WInfo.SetupArray[0].Start = 0;
-    WInfo.SetupArray[0].End = 5;
+    WInfo.SetupArray[0].Start = Start0;
+    WInfo.SetupArray[0].End = End0;
 
-    WInfo.SetupArray[1].Start = 6;
-    WInfo.SetupArray[1].End = 7;
+    WInfo.SetupArray[1].Start = Start1;
+    WInfo.SetupArray[1].End = End1;
 
-    WInfo.SetupArray[2].Start = 8;
-    WInfo.SetupArray[2].End = 9;
+    WInfo.SetupArray[2].Start = Start2;
+    WInfo.SetupArray[2].End = End2;
 
-    WInfo.SetupArray[3].Start = 10;
-    WInfo.SetupArray[3].End = 11;
+    WInfo.SetupArray[3].Start = Start3;
+    WInfo.SetupArray[3].End = End3;
 
     CRTCombinBackgArray[3] = new CRTCombinBackground( MForm, WInfo );
     ShowStatus( "Starting background process at: 3" );
     CRTCombinBackgArray[3].RunWorkerAsync( WInfo );
 
+
     }
     catch( Exception Except )
       {
       ShowStatus( "Exception in loop for starting CRTCombinBackgArray:\r\n" + Except.Message );
+      }
+    }
+
+
+
+  internal void FoundSolutionShutDown( string ProcessName,
+                                       string SolutionP,
+                                       string SolutionQ )
+    {
+    if( IsDisposed )
+      return;
+
+    ShowStatus( "Found the solution in: " + ProcessName );
+    ShowStatus( "Stopping processes." );
+    ShowStatus( "SolutionP: " + SolutionP );
+    ShowStatus( "SolutionQ: " + SolutionQ );
+
+    if( CRTCombinBackgArray == null )
+      return;
+
+    for( int Count = 0; Count < CRTCombinBackgArray.Length; Count++ )
+      {
+      try
+      {
+      if( CRTCombinBackgArray[Count] == null )
+        continue;
+
+      if( CRTCombinBackgArray[Count].IsBusy )
+        {
+        if( !CRTCombinBackgArray[Count].CancellationPending )
+          CRTCombinBackgArray[Count].CancelAsync();
+
+        }
+
+      CRTCombinBackgArray[Count].Dispose();
+      CRTCombinBackgArray[Count] = null;
+      }
+      catch( Exception Except )
+        {
+        ShowStatus( "Exception for FoundSolutionShutDown():\r\n" + Except.Message );
+        }
       }
     }
 
